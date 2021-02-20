a0c587f6e9a02e56a71fe3c7efa1f7cad2d0a574
srnutellayt <79350515+srnutellayt@users.noreply.github.com>
2021-02-20 03:25:18 -0300

    main

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (NutellaBot)
+ *
+ * Version information   (0.0.1)
+ *
+ * Date                  (20/02/2021 01:49am)
+ *
+ * author                (SrNutella)
+ * Copyright notice      (bot com suporte a Android/Java)
